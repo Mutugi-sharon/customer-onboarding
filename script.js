147e06d3b688219a23b5fe69bebdfe1344df58ef
Mutugi-sharon <sharonmutugi18@gmail.com>
2026-09-23 12:11:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,63 @@
 // START APPLICATION
 
 function startApplication() {
-  window.location.href = "register.html";
+    window.location.href = "register.html";
 }
+
 
 // GO TO LOGIN
 
 function goToLogin() {
-  window.location.href = "login.html";
+    window.location.href = "login.html";
 }
+
 
 // SCROLL TO SECTION
 
 function scrollToSection(sectionId) {
-  const section = document.getElementById(sectionId);
 
-  if (section) {
-    section.scrollIntoView({
-      behavior: "smooth",
-    });
-  }
+    const section = document.getElementById(sectionId);
+
+    if (section) {
+        section.scrollIntoView({
+            behavior: "smooth"
+        });
+    }
+
 }
+
 
 // FAQ
 
 function toggleFAQ(button) {
-  const faq = button.parentElement;
 
-  const allFAQs = document.querySelectorAll(".faq");
+    const faq = button.parentElement;
 
-  allFAQs.forEach((item) => {
-    if (item !== faq) {
-      item.classList.remove("open");
+    const allFAQs = document.querySelectorAll(".faq");
 
-      const icon = item.querySelector("span");
+    allFAQs.forEach(item => {
 
-      if (icon) {
+        if (item !== faq) {
+            item.classList.remove("open");
+
+            const icon = item.querySelector("span");
+
+            if (icon) {
+                icon.textContent = "+";
+            }
+        }
+
+    });
+
+
+    faq.classList.toggle("open");
+
+    const icon = button.querySelector("span");
+
+    if (faq.classList.contains("open")) {
+        icon.textContent = "−";
+    } else {
         icon.textContent = "+";
-      }
     }
-  });
 
-  faq.classList.toggle("open");
-
-  const icon = button.querySelector("span");
-
-  if (faq.classList.contains("open")) {
-    icon.textContent = "−";
-  } else {
-    icon.textContent = "+";
-  }
 }
